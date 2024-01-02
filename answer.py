815dc0171af5976af7f5818c272a6c7923b3c90a
Shivansh <shivansh.rawat@enine.school>
2024-01-02 22:02:04 +0530

    Assignment Submitted

--- answer.py
+++ answer.py
@@ -1,2 +1,9 @@
+"""MATH 016"""
+from solver import solver
+
+
 def answer():
-    return
+    """
+    Returns sum of digits of 2^1000
+    """
+    return solver(1000)
--- solver.py
+++ solver.py
@@ -1,2 +1,31 @@
-def solver():
-    return
+"""MATH 016"""
+
+
+def solver(n):
+    """
+    Returns sum of digits of 2^n
+    """
+    starter = "1"
+    for i in range(n):
+        carry = None
+        product = "0"
+        fprod = []
+        for j in reversed(range(len(str(starter)))):
+            if carry:
+                if j == 0:
+                    product = str((int(starter[j]) * 2) + int(carry))
+                    fprod.append(product)
+                    break
+                product = str((int(starter[j]) * 2) + int(carry))
+            else:
+                if j == 0:
+                    product = str((int(starter[j]) * 2))
+                    fprod.append(product)
+                    break
+                product = str(int(starter[j]) * 2)
+            carry = product[:-1]
+            product = product[-1]
+            fprod.append(product)
+        starter = "".join(reversed(fprod))
+
+    return sum(int(i) for i in starter)
